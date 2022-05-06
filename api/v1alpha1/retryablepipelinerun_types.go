e40531dcec401d16939eaa3daf5c937a8e045ac8
Hiroki Okui <hiroki.okui@gmail.com>
2022-05-06 12:29:29 +0900

    Add RetryablePipelineRun types and generate CRD

--- api/v1alpha1/retryablepipelinerun_types.go
+++ api/v1alpha1/retryablepipelinerun_types.go
@@ -17,26 +17,10 @@ limitations under the License.
 package v1alpha1
 
 import (
+	pipelinev1beta1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	duckv1beta1 "knative.dev/pkg/apis/duck/v1beta1"
 )
-
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
-// RetryablePipelineRunSpec defines the desired state of RetryablePipelineRun
-type RetryablePipelineRunSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of RetryablePipelineRun. Edit retryablepipelinerun_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
-}
-
-// RetryablePipelineRunStatus defines the observed state of RetryablePipelineRun
-type RetryablePipelineRunStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-}
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
@@ -46,7 +30,13 @@ type RetryablePipelineRun struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   RetryablePipelineRunSpec   `json:"spec,omitempty"`
+	// +optional
+	Spec RetryablePipelineRunSpec `json:"spec,omitempty"`
+
+	// +kubebuilder:pruning:PreserveUnknownFields
+	// +kubebuilder:validation:Schemaless
+	// +kubebuilder:validation:Type=object
+	// +optional
 	Status RetryablePipelineRunStatus `json:"status,omitempty"`
 }
 
@@ -61,4 +51,59 @@ type RetryablePipelineRunList struct {
 
 func init() {
 	SchemeBuilder.Register(&RetryablePipelineRun{}, &RetryablePipelineRunList{})
+}
+
+// RetryablePipelineRunSpec defines the desired state of RetryablePipelineRun
+type RetryablePipelineRunSpec struct {
+	pipelinev1beta1.PipelineRunSpec `json:"spec,inline"`
+}
+
+// RetryablePipelineRunStatus defines the observed state of RetryablePipelineRun
+type RetryablePipelineRunStatus struct {
+	// StartTime is the time the RetryablePipelineRun is actually started.
+	// +optional
+	StartTime *metav1.Time `json:"startTime,omitempty"`
+
+	// CompletionTime is the time the RetryablePipelineRun completed.
+	// +optional
+	CompletionTime *metav1.Time `json:"completionTime,omitempty"`
+
+	// PipelineRuns is the map of PipelineRunTaskRunStatus with the taskRun name as the key.
+	// +optional
+	PipelineRuns map[string]*pipelinev1beta1.PipelineRunStatus `json:"pipelineRuns,omitempty"`
+
+	// PipelineResults is the list of results written out by the pipeline task's containers
+	// +optional
+	// +listType=atomic
+	PipelineResults []pipelinev1beta1.PipelineRunResult `json:"pipelineResults,omitempty"`
+
+	// PinnedPipelineRun contains the exact spec used to instantiate the first run.
+	PinnedPipelineRun *pipelinev1beta1.PipelineRun `json:"pinnedPipelineRun,omitempty"`
+}
+
+// PartialPipelineRunStatusFields contain the fields of child PipelineRuns' status.
+type PartialPipelineRunStatusFields struct {
+	// StartTime is the time the PipelineRun is actually started.
+	// +optional
+	StartTime *metav1.Time `json:"startTime,omitempty"`
+
+	// CompletionTime is the time the PipelineRun completed.
+	// +optional
+	CompletionTime *metav1.Time `json:"completionTime,omitempty"`
+
+	// Conditions is the latest available observations of a resource's current state.
+	// +optional
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	Conditions duckv1beta1.Conditions `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+
+	// SkippedTasks is the list of tasks that were skipped due to when expressions evaluating to false.
+	// +optional
+	// +listType=atomic
+	SkippedTasks []pipelinev1beta1.SkippedTask `json:"skippedTasks,omitempty"`
+
+	// ChildReferences is the list of TaskRun and Run names, PipelineTask names, and API versions/kinds for children of this PipelineRun.
+	// +optional
+	// +listType=atomic
+	ChildReferences []pipelinev1beta1.ChildStatusReference `json:"childReferences,omitempty"`
 }
