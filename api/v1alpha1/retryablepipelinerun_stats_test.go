3eb830d55de6e80afa58ba0d2f0184a3fa7031a3
Hiroki Okui <hiroki.okui@gmail.com>
2022-05-22 11:43:23 +0900

    Add missing testcase of ReducedPipelineRunCondition

--- api/v1alpha1/retryablepipelinerun_stats_test.go
+++ api/v1alpha1/retryablepipelinerun_stats_test.go
@@ -20,6 +20,9 @@ import (
 	"github.com/hrk091/retryable-pipeline/api/v1alpha1"
 	"github.com/hrk091/retryable-pipeline/internal"
 	"github.com/stretchr/testify/assert"
+	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	"knative.dev/pkg/apis"
 	"testing"
 )
 
@@ -236,6 +239,157 @@ func TestNewReducedPipelineRunCondition_withPipelineRunStatuses(t *testing.T) {
 	assert.False(t, c.Tasks["task2"].Skipped)
 }
 
-func TestReducedPipelineRunCondition_Stats(t *testing.T) {
-	t.Error("TODO")
+func TestNewReducedPipelineRunCondition_Stats(t *testing.T) {
+	testCases := []struct {
+		name  string
+		given *v1alpha1.ReducedPipelineRunCondition
+		want  *v1alpha1.TaskRunStatusCount
+	}{
+		{
+			name:  "when not initialized",
+			given: nil,
+			want:  nil,
+		},
+		{
+			name: "when running",
+			given: &v1alpha1.ReducedPipelineRunCondition{
+				Tasks: map[string]v1alpha1.ReducedTaskRunCondition{
+					"task1": {
+						Skipped: true,
+					},
+					"task2": {
+						Condition: &apis.Condition{
+							Type:   apis.ConditionSucceeded,
+							Status: corev1.ConditionTrue,
+							Reason: v1beta1.TaskRunReasonSuccessful.String(),
+						},
+						Started: true,
+					},
+					"task3": {
+						Condition: &apis.Condition{
+							Type:   apis.ConditionSucceeded,
+							Status: corev1.ConditionUnknown,
+							Reason: v1beta1.TaskRunReasonRunning.String(),
+						},
+						Started: true,
+					},
+					"task4": {},
+				},
+			},
+			want: &v1alpha1.TaskRunStatusCount{
+				All:        4,
+				Skipped:    1,
+				NotStarted: 1,
+				Incomplete: 1,
+				Succeeded:  1,
+			},
+		},
+		{
+			name: "when succeeded",
+			given: &v1alpha1.ReducedPipelineRunCondition{
+				Tasks: map[string]v1alpha1.ReducedTaskRunCondition{
+					"task1": {
+						Skipped: true,
+					},
+					"task2": {
+						Condition: &apis.Condition{
+							Type:   apis.ConditionSucceeded,
+							Status: corev1.ConditionTrue,
+							Reason: v1beta1.TaskRunReasonSuccessful.String(),
+						},
+						Started: true,
+					},
+					"task3": {
+						Condition: &apis.Condition{
+							Type:   apis.ConditionSucceeded,
+							Status: corev1.ConditionTrue,
+							Reason: v1beta1.TaskRunReasonSuccessful.String(),
+						},
+						Started: true,
+					},
+					"task4": {
+						Skipped: true,
+					},
+				},
+			},
+			want: &v1alpha1.TaskRunStatusCount{
+				All:       4,
+				Skipped:   2,
+				Succeeded: 2,
+			},
+		},
+		{
+			name: "when failed",
+			given: &v1alpha1.ReducedPipelineRunCondition{
+				Tasks: map[string]v1alpha1.ReducedTaskRunCondition{
+					"task1": {
+						Skipped: true,
+					},
+					"task2": {
+						Condition: &apis.Condition{
+							Type:   apis.ConditionSucceeded,
+							Status: corev1.ConditionTrue,
+							Reason: v1beta1.TaskRunReasonSuccessful.String(),
+						},
+						Started: true,
+					},
+					"task3": {
+						Condition: &apis.Condition{
+							Type:   apis.ConditionSucceeded,
+							Status: corev1.ConditionFalse,
+							Reason: v1beta1.TaskRunReasonFailed.String(),
+						},
+						Started: true,
+					},
+					"task4": {},
+				},
+			},
+			want: &v1alpha1.TaskRunStatusCount{
+				All:        4,
+				Skipped:    1,
+				NotStarted: 1,
+				Succeeded:  1,
+				Failed:     1,
+			},
+		},
+		{
+			name: "when cancelled",
+			given: &v1alpha1.ReducedPipelineRunCondition{
+				Tasks: map[string]v1alpha1.ReducedTaskRunCondition{
+					"task1": {
+						Skipped: true,
+					},
+					"task2": {
+						Condition: &apis.Condition{
+							Type:   apis.ConditionSucceeded,
+							Status: corev1.ConditionTrue,
+							Reason: v1beta1.TaskRunReasonSuccessful.String(),
+						},
+						Started: true,
+					},
+					"task3": {
+						Condition: &apis.Condition{
+							Type:   apis.ConditionSucceeded,
+							Status: corev1.ConditionFalse,
+							Reason: v1beta1.TaskRunReasonCancelled.String(),
+						},
+						Started: true,
+					},
+					"task4": {},
+				},
+			},
+			want: &v1alpha1.TaskRunStatusCount{
+				All:        4,
+				Skipped:    1,
+				NotStarted: 1,
+				Succeeded:  1,
+				Cancelled:  1,
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.want, tc.given.Stats())
+		})
+	}
 }
